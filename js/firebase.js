3273d85def80606e876258f7b2d3f3f8aa783a54
Matheus da Silva <matheu.lpp@gmail.com>
2020-06-09 23:53:17 -0300

    firebase

---
+++ js/firebase.js
@@ -0,0 +1,13 @@
+function firebase (){
+
+    var firebaseConfig = {
+        apiKey: "AIzaSyDGfYYnw0sKTaIOVnmlB9SY7SEwMf3s2N0",
+        authDomain: "meu-projeto-5153a.firebaseapp.com",
+        databaseURL: "https://meu-projeto-5153a.firebaseio.com",
+        projectId: "meu-projeto-5153a",
+        storageBucket: "meu-projeto-5153a.appspot.com",
+        messagingSenderId: "923999082849",
+        appId: "1:923999082849:web:a04dc656be127688c14d20",
+        measurementId: "G-C70C63PJSX"
+      };
+}
